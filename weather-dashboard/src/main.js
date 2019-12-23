ac0308d982ff1e5eb87e48a64cd717fa165c5e0d
aseuna <arttuseuna@gmail.com>
2019-12-23 19:17:43 +0200

    router implemented, startes find component

--- weather-dashboard/src/main.js
+++ weather-dashboard/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -12,5 +13,6 @@ Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    render: h => h(App)
+  }).$mount('#app')
---
+++ weather-dashboard/src/views/Find.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Find'
+}
+</script>
+
+<style scoped>
+
+</style>
